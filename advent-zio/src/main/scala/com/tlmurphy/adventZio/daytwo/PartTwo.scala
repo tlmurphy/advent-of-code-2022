5a94996c4e75b34c820a63d4920a0c80b624aedc
Trevor Murphy <trevorlm95@gmail.com>
2023-07-02 12:17:48 -0500

    Refactor RoundTwo case class

--- advent-zio/src/main/scala/com/tlmurphy/adventZio/daytwo/PartTwo.scala
+++ advent-zio/src/main/scala/com/tlmurphy/adventZio/daytwo/PartTwo.scala
@@ -6,22 +6,25 @@ import zio.stream.*
 import com.tlmurphy.adventZio.FileReader
 
 case class RoundTwo(opponent: Char, me: Char):
+  private val loss: Int = opponent match
+    case 'A' => 3
+    case 'B' => 1
+    case 'C' => 2
+
+  private val tie: Int = opponent match
+    case 'A' => 1
+    case 'B' => 2
+    case 'C' => 3
+
+  private val win: Int = opponent match
+    case 'A' => 2
+    case 'B' => 3
+    case 'C' => 1
+
   def score: Int = me match
-    case 'X' =>
-      opponent match
-        case 'A' => 3
-        case 'B' => 1
-        case 'C' => 2
-    case 'Y' =>
-      opponent match
-        case 'A' => 3 + 1
-        case 'B' => 3 + 2
-        case 'C' => 3 + 3
-    case 'Z' =>
-      opponent match
-        case 'A' => 6 + 2
-        case 'B' => 6 + 3
-        case 'C' => 6 + 1
+    case 'X' => loss
+    case 'Y' => 3 + tie
+    case 'Z' => 6 + win
 
 object PartTwo extends ZIOAppDefault:
   override def run: ZIO[Any & (ZIOAppArgs & Scope), Any, Any] =
