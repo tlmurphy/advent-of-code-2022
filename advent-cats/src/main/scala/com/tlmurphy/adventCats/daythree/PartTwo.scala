30b79400d87302c9312e2f547c7a4de6ff1a13b0
Trevor Murphy <trevorlm95@gmail.com>
2023-07-03 18:10:51 -0500

    Complete day 3

---
+++ advent-cats/src/main/scala/com/tlmurphy/adventCats/daythree/PartTwo.scala
@@ -0,0 +1,28 @@
+package com.tlmurphy.adventCats.daythree
+
+import cats.effect.IOApp
+import cats.effect.IO
+import cats.syntax.*
+import com.tlmurphy.adventCats.FileReader
+import fs2.Chunk
+
+object PartTwo extends IOApp.Simple:
+
+  private case class SackGroup(sacks: Chunk[String]):
+    val priorites: Map[Char, Int] =
+      ((('a' to 'z') ++ ('A' to 'Z')) zip (1 to 52)).toMap
+    def commonItem: Char =
+      sacks.toList.reduce(_ intersect _).head
+    def prioVal(c: Char): Int = priorites(c)
+
+  override def run: IO[Unit] =
+    FileReader
+      .getStream("day3.txt")
+      .takeWhile(_ != "")
+      .chunkN(3, false)
+      .map(SackGroup(_))
+      .map(s => s.prioVal(s.commonItem))
+      .reduce(_ + _)
+      .evalMap(prioSum => IO(println(prioSum)))
+      .compile
+      .drain
